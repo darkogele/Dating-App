5c7a8b89c6b9c38f08f5fa5841d31afcf78e7a5f
darko.gelevski <darkogele@hotmail.com>
2024-06-24 10:50:21 +0200

    End of section 7

---
+++ DatingApp/API/Controllers/BuggyController.cs
@@ -0,0 +1,41 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController(DataContext context) : BaseController
+{
+    [Authorize]
+    [HttpGet("auth")]
+    public ActionResult<string> GetSecret()
+    {
+        return "Secret text";
+    }
+
+    [HttpGet("not-found")]
+    public ActionResult<AppUser> GetNotFound()
+    {
+        var thing = context.Users.Find(-1);
+
+        if (thing == null) return NotFound();
+
+        return Ok(thing);
+    }
+
+    [HttpGet("server-error")]
+    public ActionResult<AppUser> GetServerError()
+    {
+        var thing = context.Users.Find(-1)
+            ?? throw new Exception("A bad thing happened");
+
+        return thing;
+    }
+
+    [HttpGet("bad-request")]
+    public ActionResult<string> GetBadRequest()
+    {
+        return BadRequest("This was not a good request");
+    }
+}
